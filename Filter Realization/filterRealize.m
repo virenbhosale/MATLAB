376419932f6a6dc3287fbbda4a2080be0f1c19dd
Viren Bhosale <62363546+virenbhosale@users.noreply.github.com>
2020-04-15 15:01:58 -0500

    Update filterRealize.m

--- Filter Realization/filterRealize.m
+++ Filter Realization/filterRealize.m
@@ -1,15 +1,14 @@
 clear
 clc
 
-% IMPORT DATA
-load maximallyFlatLowPass.txt
+load butterworth.txt
 load chebyshev0_5.txt
 load chebyshev3.txt
 
 % SELECT FILTER
-lp = 0; 
+lp = 1; 
 hp = 0;
-bp = 1;
+bp = 0;
 % SELECT RESPONSE
 b = 0; % Butterworth Maximally Flat
 c = 1; % 0.5 dB Ripple
@@ -17,141 +16,179 @@ c3db = 0; % 3 dB Ripple
 
 %% PARAMETERS
 fc = 1E9; % CUT-OFF FREQUENCY
-N = 5; % ORDER
-Rs = 50; % SOURCE IMPEDANCE*
-flo = 1.8E9;
-fhi = 2.1E9;
+N = 8; % ORDER
+Ro = 50; % CHARACTERISTIC IMPEDANCE
+flo = 1.9E9; % PART OF THE PASS BAND
+fhi = 2.0E9;
 
-BW = fhi - flo;
+BW = 2*pi*(fhi - flo);
 wlo = 2*pi*flo;
 whi = 2*pi*fhi;
 wc = 2*pi*fc;
 wo = sqrt(wlo*whi);
-values = zeros(1, 10);
-
+values = zeros(2, 10);
+valuesbp = zeros(4, 10);
 %% VALUES FOR LOW PASS
 % FOR LOW PASS START SERIES L, then SHUNT C, terminated at RL
+% COLUMN 1: Series Inductor then Shunt Capacitor
+% COLUMN 2: Shunt Capacitor then Series Inductor
 if lp == 1 && b == 1% LOW PASS MAXIMALLY FLAT | BUTTERWORTH
     for i = 1:N
         if (rem(i,2) ~= 0)
-            values(i) = Rs * maximallyFlatLowPass(N, i)/wc;
+            values(1, i) = Ro * butterworth(N, i)/wc;
+            values(2, i) = butterworth(N, i)/(wc*Ro);
         else
-            values(i) = maximallyFlatLowPass(N, i)/(wc*Rs);
+            values(1, i) = butterworth(N, i)/(wc*Ro);
+            values(2, i) = Ro * butterworth(N, i)/wc;
         end
     end
     
-    values(N+1) = Rs * maximallyFlatLowPass(N, N+1);
-    values = values(values>0);
+    values(1, N+1) = Ro * butterworth(N, N+1);
+    values(2, N+1) = Ro * butterworth(N, N+1);
+    values = values.';
 end
 
 if lp == 1 && c == 1% LOW PASS w/ cheby 0.5 dB
     for i = 1:N
         if (rem(i,2) ~= 0)
-            values(i) = Rs * chebyshev0_5(N, i)/wc;
+            values(1, i) = Ro * chebyshev0_5(N, i)/wc;
+            values(2, i) = chebyshev0_5(N, i)/(wc*Ro);
         else
-            values(i) = chebyshev0_5(N, i)/(wc*Rs);
+            values(1, i) = chebyshev0_5(N, i)/(wc*Ro);
+            values(2, i) = Ro * chebyshev0_5(N, i)/wc;
         end
     end
     
-    values(N+1) = Rs * chebyshev0_5(N, N+1);
-    values = values(values>0);
+    values(1, N+1) = Ro * chebyshev0_5(N, N+1);
+    values(2, N+1) = Ro * chebyshev0_5(N, N+1);
+    values = values.';
 end
 
 if lp == 1 && c3db == 1% LOW PASS w/ cheby 3 dB
     for i = 1:N
         if (rem(i,2) ~= 0)
-            values(i) = Rs * chebyshev3(N, i)/wc;
+            values(1, i) = Ro * chebyshev3(N, i)/wc;
+            values(2, i) = chebyshev3(N, i)/(wc*Ro);
         else
-            values(i) = chebyshev3(N, i)/(wc*Rs);
+            values(1, i) = chebyshev3(N, i)/(wc*Ro);
+            values(2, i) = Ro * chebyshev3(N, i)/wc;
         end
     end
     
-    values(N+1) = Rs * chebyshev3(N, N+1);
-    values = values(values>0);
+    values(1, N+1) = Ro * chebyshev3(N, N+1);
+    values(2, N+1) = Ro * chebyshev3(N, N+1);
+    values = values.';
 end
 
 %% VALUES FOR HIGH PASS
-% FOR HIGH PASS START SERIES C, then SHUNT L, terminated at RL
+% COLUMN 1: SERIES C then SHUNT L
+% COULMN 2: SHUNT L then SERIES C
 if hp == 1 && b == 1
     for i = 1:N
         if(rem(i,2) ~= 0)
-            values(i) = 1/(wc*maximallyFlatLowPass(N, i)*Rs);
+            values(1, i) = 1/(wc*butterworth(N, i)*Ro);
+            values(2, i) = Ro * 1/(wc*butterworth(N, i));
         else
-            values(i) = Rs * 1/(wc*maximallyFlatLowPass(N, i));
+            values(1, i) = Ro * 1/(wc*butterworth(N, i));
+            values(2, i) = 1/(wc*butterworth(N, i)*Ro);
         end
     end
     
-    values(N+1) = Rs * maximallyFlatLowPass(N, N+1);
-    values = values(values>0);
-    
+    values(1, N+1) = Ro * butterworth(N, N+1);
+    values(2, N+1) = Ro * butterworth(N, N+1);
+    values = values.';
 end
 
 if hp == 1 && c == 1
     for i = 1:N
         if(rem(i,2) ~= 0)
-            values(i) = 1/(wc*chebyshev0_5(N, i)*Rs);
+            values(1, i) = 1/(wc*chebyshev0_5(N, i)*Ro);
+            values(2, i) = Ro * 1/(wc*chebyshev0_5(N, i));
         else
-            values(i) = Rs * 1/(wc*chebyshev0_5(N, i));
+            values(1, i) = Ro * 1/(wc*chebyshev0_5(N, i));
+            values(2, i) = 1/(wc*chebyshev0_5(N, i)*Ro);
         end
     end
     
-    values(N+1) = Rs * chebyshev0_5(N, N+1);
-    values = values(values>0);
+    values(1, N+1) = Ro * chebyshev0_5(N, N+1);
+    values(2, N+1) = Ro * chebyshev0_5(N, N+1);
+    values = values.';
     
 end
 
 if hp == 1 && c3db == 1
     for i = 1:N
         if(rem(i,2) ~= 0)
-            values(i) = 1/(wc*chebyshev3(N, i)*Rs);
+            values(1, i) = 1/(wc*chebyshev3(N, i)*Ro);
+            values(2, i) = Ro * 1/(wc*chebyshev3(N, i));
         else
-            values(i) = Rs * 1/(wc*chebyshev3(N, i));
+            values(1, i) = Ro * 1/(wc*chebyshev3(N, i));
+            values(2, i) = 1/(wc*chebyshev3(N, i)*Ro);
         end
     end
    
-    values(N+1) = Rs * chebyshev3(N, N+1);
-    values = values(values>0);
+    values(1, N+1) = Ro * chebyshev3(N, N+1);
+    values(2, N+1) = Ro * chebyshev3(N, N+1);
+    values = values.';
 end
 
 %% VALUES FOR BAND PASS
-% FOR BAND PASS START SERIES L + C, then SHUNT L + SHUNT C
-% ROW 1 is INDUCTOR, ROW 2 is CAPACITOR, Final Column has RL Value
-valuesbp = zeros(2, 10);
+% COL 1 is INDUCTOR, COL 2 is CAPACITOR (SERIES L + SERIES C COMBO FIRST)
+% COL 3 is INDUCTOR, COL 4 is CAPACITOR (SHUNT L + SHUNT C COMBO FIRST)
+% COLS 1, 2 are linked | COLS 3, 4 are linked
 if bp == 1 && b == 1
     for i = 1:N
        if(rem(i,2) ~= 0)
-          valuesbp(1,i) = maximallyFlatLowPass(N, i) / BW;
-          valuesbp(2,i) = BW / (wo^2 * maximallyFlatLowPass(N, i));
+          valuesbp(1,i) = Ro * butterworth(N, i) / BW;
+          valuesbp(2,i) = BW / (Ro * wo^2 * butterworth(N, i));
+          valuesbp(3,i) = Ro * BW / (wo^2 * butterworth(N, i));
+          valuesbp(4,i) = butterworth(N, i) / (Ro * BW);
        else
-          valuesbp(1,i) = BW / (wo^2 * maximallyFlatLowPass(N, i));
-          valuesbp(2,i) = maximallyFlatLowPass(N, i) / BW;
+          valuesbp(1,i) = Ro * BW / (wo^2 * butterworth(N, i));
+          valuesbp(2,i) = butterworth(N, i) / (Ro * BW);
+          valuesbp(3,i) = Ro * butterworth(N, i) / BW;
+          valuesbp(4,i) = BW / (Ro * wo^2 * butterworth(N, i));
        end
     end
-    valuesbp(1,N+1) = Rs * maximallyFlatLowPass(N, N+1);
+    valuesbp(1,N+1) = Ro * butterworth(N, N+1);
+    valuesbp(3,N+1) = Ro * butterworth(N, N+1);
+    valuesbp = valuesbp.';
 end
 
 if bp == 1 && c == 1
     for i = 1:N
        if(rem(i,2) ~= 0)
-          valuesbp(1,i) = chebyshev0_5(N, i) / BW;
-          valuesbp(2,i) = BW / (wo^2 * chebyshev0_5(N, i));
+          valuesbp(1,i) = Ro * chebyshev0_5(N, i) / BW;
+          valuesbp(2,i) = BW / (Ro * wo^2 * chebyshev0_5(N, i));
+          valuesbp(3,i) = Ro * BW / (wo^2 * chebyshev0_5(N, i));
+          valuesbp(4,i) = chebyshev0_5(N, i) / (Ro * BW);
        else
-          valuesbp(1,i) = BW / (wo^2 * chebyshev0_5(N, i));
-          valuesbp(2,i) = chebyshev0_5(N, i) / BW;
+          valuesbp(1,i) = Ro * BW / (wo^2 * chebyshev0_5(N, i));
+          valuesbp(2,i) = chebyshev0_5(N, i) / (Ro * BW);
+          valuesbp(3,i) = Ro * chebyshev0_5(N, i) / BW;
+          valuesbp(4,i) = BW / (Ro * wo^2 * chebyshev0_5(N, i));
        end
     end
-    valuesbp(1,N+1) = Rs * chebyshev0_5(N, N+1);
+    valuesbp(1,N+1) = Ro * chebyshev0_5(N, N+1);
+    valuesbp(3,N+1) = Ro * chebyshev0_5(N, N+1);
+    valuesbp = valuesbp.';
 end
 
 if bp == 1 && c3db == 1
     for i = 1:N
        if(rem(i,2) ~= 0)
-          valuesbp(1,i) = chebyshev3(N, i) / BW;
-          valuesbp(2,i) = BW / (wo^2 * chebyshev3(N, i));
+          valuesbp(1,i) = Ro * chebyshev3(N, i) / BW;
+          valuesbp(2,i) = BW / (Ro * wo^2 * chebyshev3(N, i));
+          valuesbp(3,i) = Ro * BW / (wo^2 * chebyshev3(N, i));
+          valuesbp(4,i) = chebyshev3(N, i) / (Ro * BW);
        else
-          valuesbp(1,i) = BW / (wo^2 * chebyshev3(N, i));
-          valuesbp(2,i) = chebyshev3(N, i) / BW;
+          valuesbp(1,i) = Ro * BW / (wo^2 * chebyshev3(N, i));
+          valuesbp(2,i) = chebyshev3(N, i) / (Ro * BW);
+          valuesbp(3,i) = Ro * chebyshev3(N, i) / BW;
+          valuesbp(4,i) = BW / (Ro * wo^2 * chebyshev3(N, i));
        end
     end
-    valuesbp(1,N+1) = Rs * chebyshev3(N, N+1);
+    valuesbp(1,N+1) = Ro * chebyshev3(N, N+1);
+    valuesbp(3,N+1) = Ro * chebyshev3(N, N+1);
+    valuesbp = valuesbp.';
 end
